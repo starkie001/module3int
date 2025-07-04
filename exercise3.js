841ec931c4039137109290d3eb93d70e9c404d03
starkie001 <starkwanos@hotmail.com>
2025-07-05 09:49:44 +1200

    exercise6, function for removing duplicates from array.

--- exercise3.js
+++ exercise3.js
@@ -1,7 +1,18 @@
-const animals = ['Tiger', 'Giraffe'];
+let animals = ['Tiger', 'Giraffe'];
 
 animals.push('Yak', 'Zebra');
 animals.unshift('Aardvark', 'Anteater');
 animals.sort((a, b) => a.localeCompare(b));
 
-console.log(animals)
+function replaceMiddleAnimal(newValue){
+    if (animals.length % 2 === 0) {  //is even
+        animals.splice(animals.length % 2, 2, newValue)
+    }
+    Else {
+        animals.splice(animals.length + .5, 1, newValue);
+    }
+    return animals;
+}
+
+console.log(animals);
+console.log(replaceMiddleAnimal('Pig'));
---
+++ exercise6.js
@@ -0,0 +1,12 @@
+function unique(duplicateArray) {
+    const uniqueArray = new Set(duplicateArray);
+    return uniqueArray;
+}
+
+const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow']
+const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43]
+const animals = ['sheep', 'sheep', 'sheep', 'dog', 'cow', 'cow', 'dogfish']
+
+console.log(unique(colours)) // [ 'red', 'green', 'blue', 'yellow', 'orange' ]
+console.log(unique(testScores)) // [ 55, 84, 97, 63, 32, 91, 43 ]
+console.log(unique(animals))
